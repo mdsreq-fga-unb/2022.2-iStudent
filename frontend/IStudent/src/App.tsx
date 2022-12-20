1914c14e4254fb356f29940ec044927d42c6e537
lucascaldasb <lucas20caldas@gmail.com>
2022-12-20 17:14:25 -0300

    Iniciando frontend do projeto

---
+++ frontend/IStudent/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>iStudent</h1>
+    </div>
+  );
+}
+
+export default App;
