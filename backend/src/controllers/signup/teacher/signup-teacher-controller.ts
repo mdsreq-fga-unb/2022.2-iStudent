7c42888e1ae2234d0203cf824207e8a0e9cefded
Weslin-0101 <wesleeylirac12@gmail.com>
2022-12-27 17:02:04 -0300

    feat: garantindo que SignUpTeacherController retorne uma conta

--- backend/src/controllers/signup/teacher/signup-teacher-controller.ts
+++ backend/src/controllers/signup/teacher/signup-teacher-controller.ts
@@ -1,9 +1,12 @@
-import { badRequest, ok, serverError } from "../../helpers/http/http-helper";
+import { AddTeacherAccount } from "../../../domain/useCases/addTeacherAccount";
+import { EmailInUseError } from "../../errors";
+import { badRequest, forbidden, ok, serverError } from "../../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { Validation } from "../../protocols/validation";
 
 export class SignUpTeacherController implements Controller {
     constructor(
+        private readonly _addTeacherAccount: AddTeacherAccount,
         private readonly _validation: Validation
     ) {}
     
@@ -13,6 +16,31 @@ export class SignUpTeacherController implements Controller {
             if (error) {
                 return badRequest(error)
             }
+
+            const { 
+                name,
+                email,
+                password,
+                photo_url,
+                whatsapp,
+                biography,
+                materials,
+                costPerHour,
+                disponibility
+            } = httpRequest.body
+            const account = await this._addTeacherAccount.addTeacher({
+                name,
+                email,
+                password,
+                photo_url,
+                whatsapp,
+                biography,
+                materials,
+                costPerHour,
+                disponibility
+            })
+
+            if (!account) return forbidden(new EmailInUseError())
 
             return ok({ data: 'ok' })
         } catch (error) {
