d6e03f9573c73a310e9a6644c32eeddc344acfc8
Weslin-0101 <wesleeylirac12@gmail.com>
2022-12-27 17:01:15 -0300

    feat: criado novos erros e funções para o http-helper

---
+++ backend/src/controllers/errors/index.ts
@@ -0,0 +1,4 @@
+export * from './email-in-use.error'
+export * from './invalid-params.error'
+export * from './missign-param-errors'
+export * from './server.error'
--- backend/src/controllers/helpers/http/http-helper.ts
+++ backend/src/controllers/helpers/http/http-helper.ts
@@ -6,6 +6,11 @@ export const badRequest = (error: Error): HttpResponse => ({
     body: error
 })
 
+export const forbidden = (error: Error): HttpResponse => ({
+    statusCode: 403,
+    body: error
+})
+
 export const serverError = (error: Error): HttpResponse => ({
     statusCode: 500,
     body: new ServerError(error.stack)
