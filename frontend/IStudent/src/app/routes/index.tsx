c7972201c2b45a03e39993c9d9a08f5382f348c4
lucascaldasb <lucas20caldas@gmail.com>
2022-12-27 18:53:53 -0300

    feat: criando autenticação de usuário

--- frontend/IStudent/src/app/routes/index.tsx
+++ frontend/IStudent/src/app/routes/index.tsx
@@ -1,5 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home, SignUp, SignUpTeacher, SignUpStudent } from "../pages/index";
+import {
+  Home,
+  SignUp,
+  SignUpTeacher,
+  SignUpStudent,
+  Login,
+  ForgotPassword,
+} from "../pages/index";
 
 export const Router = () => {
   return (
@@ -9,6 +16,8 @@ export const Router = () => {
         <Route path="/cadastro" element={<SignUp />} />
         <Route path="/cadastro-professor" element={<SignUpTeacher />} />
         <Route path="/cadastro-estudante" element={<SignUpStudent />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/senha-esquecida" element={<ForgotPassword />} />
 
         <Route path="*" element={<Navigate to="/pagina-inicial" />} />
       </Routes>
