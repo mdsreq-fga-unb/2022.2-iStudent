b7f0dc7958d31cfa67eb7fdaa81e8ce0ee3fc12c
Weslin-0101 <wesleeylirac12@gmail.com>
2022-12-26 22:13:06 -0300

    feat: garantindo que SignUpTeacherController retornasse erro caso faltasse parametros

--- backend/src/controllers/signup/teacher/signup-teacher-controller.ts
+++ backend/src/controllers/signup/teacher/signup-teacher-controller.ts
@@ -1,10 +1,33 @@
+import { MissingParamError } from "../../errors/missign-param-errors";
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 
 export class SignUpTeacherController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        return new Promise(resolve => resolve({
+        const requiredField = [
+            'name',
+            'email',
+            'password',
+            'photo_url',
+            'whatsapp',
+            'biography',
+            'materials',
+            'costPerHour',
+            'disponibility'
+        ]
+        for (const field of requiredField) {
+            if (!httpRequest.body[field]) {
+                return {
+                    statusCode: 400,
+                    body: new MissingParamError(field)
+                }
+            }
+        }
+
+        return {
             statusCode: 200,
-            body: {}
-        }))
+            body: {
+                message: 'ok'
+            }
+        }
     }
 }
