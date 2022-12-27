2830f36d8720bce3a0ff06dff60460505c56d1e9
Weslin-0101 <wesleeylirac12@gmail.com>
2022-12-27 15:50:47 -0300

    feat: adicionado os middlewares

---
+++ backend/src/main/config/routes.ts
@@ -0,0 +1,10 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+
+export default (app: Express): void => {
+    const router = Router()
+    app.use('/api', router)
+    readdirSync(`${__dirname}/../routes`).map(async file => {
+        if (!file.includes('.test.')) (await import (`../routes/${file}`)).default(router)
+    })
+}
---
+++ backend/src/main/middlewares/content-type.ts
@@ -0,0 +1,6 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const contentType = (req: Request, res: Response, next: NextFunction) => {
+    res.type('json')
+    next()
+}
