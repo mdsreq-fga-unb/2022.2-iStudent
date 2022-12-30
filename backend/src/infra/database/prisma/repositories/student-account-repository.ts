9dc039710764a59655c18c261e0a57b8f04a8702
Weslin-0101 <wesleeylirac12@gmail.com>
2022-12-30 18:23:13 -0300

    feat: criado StudentAccountPrismaRepository

---
+++ backend/src/infra/database/prisma/repositories/student-account-repository.ts
@@ -0,0 +1,20 @@
+import { AddStudentRepository } from "../../../../database/repositories/student-repository";
+import { AccountStudentModel } from "../../../../domain/models/account";
+import { addStudentAccountModel } from "../../../../domain/useCases/addStudentAccount";
+import { prisma } from "@prisma/client";
+
+export class StudentAccountPrismaRepository implements AddStudentRepository {
+    async addStudent(account: addStudentAccountModel): Promise<AccountStudentModel> {
+        await prisma.student.create({
+            data: {
+                name: account.name,
+                email: account.email,
+                password: account.password,
+                whatsapp: account.whatsapp,
+                biography: account.biography
+            }
+        })
+
+        return
+    }
+}
