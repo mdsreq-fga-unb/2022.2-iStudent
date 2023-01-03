21d1da016eb4e061374d0d656378e0e80ec1ee6c
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-03 19:34:44 -0300

    chore: remove controller desnecessarios

--- backend/src/main/factories/signup-student/signup-student-controller-factory.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "../../../controllers/protocols";
-import { SignUpStudentController } from "../../../controllers/signup/student/signup-student-controller";
-import { makeSignUpStudentAccount } from "../useCases/add-student-account/signup-student-factory";
-import { makeSignUpStudentValidation } from "./signup-student-validation-factory";
-
-export const makeSignUpStudentController = (): Controller => {
-    const controller = new SignUpStudentController(makeSignUpStudentAccount(), makeSignUpStudentValidation())
-    return controller
-}
--- backend/src/main/factories/useCases/add-student-account/signup-student-factory.ts
+++
@@ -1,8 +0,0 @@
-import { CreateStudentUseCaseRepository } from "../../../../database/useCases/create-student.use-case";
-import { AddStudentAccount } from "../../../../domain/useCases/addStudentAccount";
-import { StudentAccountPrismaRepository } from "../../../../infra/database/prisma/repositories/student-account-repository";
-
-export const makeSignUpStudentAccount = (): AddStudentAccount => {
-    const addStudentAccountPrismaRepository = new StudentAccountPrismaRepository
-    return new CreateStudentUseCaseRepository(addStudentAccountPrismaRepository)
-}
