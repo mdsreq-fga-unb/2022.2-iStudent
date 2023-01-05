a3cdd5d8ea29deb77e800ac569d8c8ae1336fedf
Weslin-0101 <wesleeylirac12@gmail.com>
2023-01-04 23:01:51 -0300

    feat: criado a rota para atualizar conta

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { adaptRoute } from '../main/adapters/express/express-route.adapter';
 import { makeDeleteAccountController } from '../main/factories/deleteAccount/delete-account-factory';
+import { makeEditAccountController } from '../main/factories/editAccount/edit-account-factory';
 import { makeLoginController } from '../main/factories/login-controller-factory';
 import { makeMeController } from '../main/factories/me/me-controller-factory';
 import { makeSignUpController } from '../main/factories/signup/signup-controller-factory';
@@ -19,5 +20,7 @@ routes.post('/login', adaptRoute(makeLoginController()));
 
 routes.get('/me', auth(), adaptRoute(makeMeController()));
 routes.delete('/me', auth(), adaptRoute(makeDeleteAccountController()));
+
+routes.get('/uptade-account', adaptRoute(makeEditAccountController()));
 
 export default routes;
