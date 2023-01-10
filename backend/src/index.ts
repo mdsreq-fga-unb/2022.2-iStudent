a344136e6f781550372815b1dab0c85294e32fce
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-10 13:08:56 -0300

    chore: log da connection com db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const server = app.listen(PORT, () =>
-  console.log(`🚀 Server ready at: http://localhost:${PORT}`),
-);
+const server = app.listen(PORT, () => {
+  prisma.$connect().then(() => console.log("Connected to the DB"))
+  console.log(`🚀 Server ready at: http://localhost:${PORT}`)
+});
 
 process.on('SIGINT', async () => {
   try {
