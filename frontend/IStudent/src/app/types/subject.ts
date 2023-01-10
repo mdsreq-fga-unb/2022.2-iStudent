a9e652094390effa7944199c111f6be68047ec45
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-10 13:11:12 -0300

    feat: subject e teacher type

---
+++ frontend/IStudent/src/app/types/subject.ts
@@ -0,0 +1,5 @@
+export type Subject = {
+    name: string;
+    teacherId: number;
+    id: number;
+}
--- frontend/IStudent/src/app/types/user.ts
+++ frontend/IStudent/src/app/types/user.ts
@@ -1,1 +1,12 @@
-export type UserRole = 'STUDENT' | 'TEACHER'    
+import { Subject } from "./subject"
+
+export type UserRole = 'STUDENT' | 'TEACHER' 
+
+export interface Teacher {
+    name: string
+    biography: string
+    role: 'TEACHER'
+    subject: Subject
+    email: string
+    createdAt: string
+}
