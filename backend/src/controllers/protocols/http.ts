457c0da8d5bb2963671413e33835a8f94b28ada4
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-12 01:31:41 -0300

    chore: formatando

--- backend/src/controllers/protocols/http.ts
+++ backend/src/controllers/protocols/http.ts
@@ -1,16 +1,16 @@
-import { UserRole } from "../../types/user"
+import { UserRole } from '../../types/user';
 
 export interface HttpResponse {
-    statusCode: number
-    body: any
+  statusCode: number;
+  body: any;
 }
 
 export interface HttpRequest {
-    body?: any
-    headers?: any
-    user?: {
-        email: string,
-        role: UserRole,
-        id: number,
-    }
+  body?: any;
+  headers?: any;
+  user?: {
+    email: string;
+    role: UserRole;
+    id: number;
+  };
 }
