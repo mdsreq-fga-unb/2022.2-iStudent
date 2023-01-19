18a1df93552b549f4b5a2681e2806e3949860d49
Weslin-0101 <wesleeylirac12@gmail.com>
2023-01-19 11:19:43 -0300

    feat: adicionado validação nos campos do CourseManagementController

--- backend/src/controllers/courseManagement/course-management-controller.ts
+++ backend/src/controllers/courseManagement/course-management-controller.ts
@@ -1,14 +1,21 @@
 import { AddCourse } from "../../domain/useCases/courseManagement/addCourse";
-import { ok, serverError } from "../helpers/http/http-helper";
+import { badRequest, ok, serverError } from "../helpers/http/http-helper";
 import { Controller, HttpRequest, HttpResponse } from "../protocols";
+import { Validation } from "../protocols/validation";
 
 export class CourseManagementController implements Controller {
     constructor(
-        private readonly _addCourse: AddCourse
+        private readonly _addCourse: AddCourse,
+        private readonly _validation: Validation
     ) {}
     
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
+            const error = this._validation.validate(httpRequest.body);
+            if (error) {
+                return badRequest(error);
+            }
+
             const { 
                 name, 
                 description, 
--- backend/src/main/factories/addCourse/add-course-controller-factory.ts
+++ backend/src/main/factories/addCourse/add-course-controller-factory.ts
@@ -1,8 +1,9 @@
 import { CourseManagementController } from "../../../controllers/courseManagement/course-management-controller"
 import { Controller } from "../../../controllers/protocols";
 import { makeAddCoursePrisma } from "../useCases/add-course-factory"
+import { makeAddCourseValidation } from "./add-course-validation-factory";
 
 export const makeAddCourseController = (): Controller => {
-    const controller = new CourseManagementController(makeAddCoursePrisma())
+    const controller = new CourseManagementController(makeAddCoursePrisma(), makeAddCourseValidation())
     return controller;
 }
