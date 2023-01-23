a13f290d0faea8d8ed1dd00cebbefb389ff98810
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-23 15:44:15 -0300

    feat: class por prof

--- backend/src/main/factories/class/get-class-by-teacher-factory.ts
+++ backend/src/main/factories/class/get-class-by-teacher-factory.ts
@@ -1,8 +1,6 @@
 import { ClassesByTeacherRequestController } from '../../../controllers/class/classes-by-teacher-controller';
-import { AddClassRequestController } from '../../../controllers/classRequest/add-class-request-controller';
 import { Controller } from '../../../controllers/protocols';
 import { ClassesPrismaRepository } from '../../../infra/database/prisma/repositories/class-repository';
-import { makePrismaClassRequest } from '../useCases/add-class-request-factory';
 
 export const makeClassesByTeacherController = (): Controller => {
   const controller = new ClassesByTeacherRequestController(
