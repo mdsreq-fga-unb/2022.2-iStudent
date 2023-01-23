17e52a5f07ea552f19f670d6b727d4f0913d911a
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-23 15:45:17 -0300

    feat: aceitar ou recusar aula

--- frontend/IStudent/src/app/shared/components/requestSchedule/RequestSchedule.tsx
+++ frontend/IStudent/src/app/shared/components/requestSchedule/RequestSchedule.tsx
@@ -1,21 +1,59 @@
 import { Container } from './styles';
 import Tippy from '@tippyjs/react';
 import { DetailCard } from '../index';
+import acceptOrDenyClass from '../../services/teachers/acceptOrDenyClass';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
 
 interface Props {
+  id: number;
   name: string;
   type: string;
   time: string;
+  accepted: boolean | null;
 }
 
-export const RequestSchedule = ({ name, time, type }: Props) => {
+export const RequestSchedule = ({ name, time, type, accepted, id }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isAccepted, setIsAccepted] = useState(accepted);
+
+  async function handleClass(accept: boolean) {
+    try {
+      setIsLoading(true);
+      await acceptOrDenyClass(accept, id);
+      setIsLoading(false);
+      alert(`Aula ${accept ? 'aceita' : 'recusada'} com sucesso!`);
+      setIsAccepted(accept);
+    } catch (error) {
+      setIsLoading(false);
+      alert('Erro ao salvar classe, tente novamente!');
+    }
+  }
   return (
     <Container>
       <div className="content">
         <h5>{name}</h5>
         <div className="button-area">
-          <button className="accept">Aceitar</button>
-          <button className="refuse">Recusar</button>
+          {isAccepted === null ? (
+            <>
+              {isLoading ? (
+                <CircularProgress color="inherit" size={30} />
+              ) : (
+                <>
+                  <button className="accept" onClick={() => handleClass(true)}>
+                    Aceitar
+                  </button>
+                  <button className="refuse" onClick={() => handleClass(false)}>
+                    Recusar
+                  </button>
+                </>
+              )}
+            </>
+          ) : isAccepted ? (
+            <div className="accepted">Aceito</div>
+          ) : (
+            <div className="refused">Recusado</div>
+          )}
           <Tippy
             content={<DetailCard time={time} type={type} />}
             interactive={true}
