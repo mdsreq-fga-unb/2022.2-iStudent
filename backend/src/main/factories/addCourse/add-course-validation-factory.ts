f6c0be8e460ded81c73acf6fe743237523977055
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-24 19:29:18 -0300

    chore: match db schema

--- backend/src/main/factories/addCourse/add-course-validation-factory.ts
+++ backend/src/main/factories/addCourse/add-course-validation-factory.ts
@@ -1,10 +1,22 @@
-import { Validation } from "../../../controllers/protocols/validation";
-import { RequiredFieldValidation, ValidationComposite } from "../../../validation/validators";
+import { Validation } from '../../../controllers/protocols/validation';
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+} from '../../../validation/validators';
 
 export const makeAddCourseValidation = (): ValidationComposite => {
-    const validations: Validation[] = [];
-    for (const field of ['name', 'description', 'price', 'contents', 'courseRating', 'teacherName', 'teacherTotalStudents', 'teacherTotalHours']) {
-        validations.push(new RequiredFieldValidation(field))
-    }
-    return new ValidationComposite(validations)
-}
+  const validations: Validation[] = [];
+  for (const field of [
+    'name',
+    'description',
+    'price',
+    'contents',
+    'courseRating',
+    'teacherName',
+    'totalStudents',
+    'totalHours',
+  ]) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+  return new ValidationComposite(validations);
+};
--- backend/src/main/factories/editCourse/edit-course-validation-factory.ts
+++ backend/src/main/factories/editCourse/edit-course-validation-factory.ts
@@ -1,10 +1,21 @@
-import { Validation } from "../../../controllers/protocols/validation";
-import { RequiredFieldValidation, ValidationComposite } from "../../../validation/validators";
+import { Validation } from '../../../controllers/protocols/validation';
+import {
+  RequiredFieldValidation,
+  ValidationComposite,
+} from '../../../validation/validators';
 
 export const makeEditCourseValidation = (): ValidationComposite => {
-    const validations: Validation[] = [];
-    for (const field of ['description', 'price', 'contents', 'courseRating', 'teacherName', 'teacherTotalStudents', 'teacherTotalHours']) {
-        validations.push(new RequiredFieldValidation(field))
-    }
-    return new ValidationComposite(validations)
-}
+  const validations: Validation[] = [];
+  for (const field of [
+    'description',
+    'price',
+    'contents',
+    'courseRating',
+    'teacherName',
+    'totalStudents',
+    'totalHours',
+  ]) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+  return new ValidationComposite(validations);
+};
