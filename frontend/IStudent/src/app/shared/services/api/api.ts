7a07318d9aee2c91a64776d79335d7b47e388803
bruno henrique <bh.bsb2011@hotmail.com>
2023-01-31 12:08:29 -0300

    chore: fix type

--- frontend/IStudent/src/app/shared/services/api/api.ts
+++ frontend/IStudent/src/app/shared/services/api/api.ts
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?  import.meta.env.VITE_API_URL : "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_URL
+    ? import.meta.env.VITE_API_URL
+    : 'http://localhost:3001',
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("ISTUDENT:TOKEN")
-  if(!token) return config
+api.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem('ISTUDENT:TOKEN');
+  if (!token) return config;
   return {
     ...config,
-    headers: { Authorization: `Bearer ${token}` }
-  }
-})
+    headers: { Authorization: `Bearer ${token}` },
+  };
+});
 
-export {
-  api
-}
+export { api };
