fdfa40bfd54f4da9c45ac3037abed22e110629e3
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-03 09:29:17 -0300

    fix: resolvendo bug da resposta do status code

--- backend/src/main/config/routes.ts
+++ backend/src/main/config/routes.ts
@@ -5,6 +5,6 @@ export default (app: Express): void => {
     const router = Router()
     app.use('/api', router)
     readdirSync(`${__dirname}/../../routes`).map(async file => {
-        if (!file.includes('.test.')) (await import (`../../routes/${file}`)).default(router)
+        if (!file.includes('.test.') && !file.endsWith('.map')) (await import (`../../routes/${file}`)).default(router)
     })
 }
