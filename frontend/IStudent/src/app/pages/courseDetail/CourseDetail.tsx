9d52d61ce79c2ef38581293e7ddd948bad042380
bruno henrique <bh.bsb2011@hotmail.com>
2023-02-05 17:22:56 -0300

    feat:  add course detail and edit page

--- frontend/IStudent/src/app/pages/courseDetail/CourseDetail.tsx
+++ frontend/IStudent/src/app/pages/courseDetail/CourseDetail.tsx
@@ -1,152 +1,95 @@
-import { CoursesCard, HeaderUser } from '../../shared/components'
-import {Container, Divisor, FirstBody, OtherCourses, SecondBody} from './styles'
-import Carousel from 'react-bootstrap/Carousel';
+import { TextField } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useUser } from '../../contexts/User';
+import { Button, HeaderUser } from '../../shared/components';
+import deleteCourse from '../../shared/services/teachers/deleteCourse';
+import getCourse from '../../shared/services/teachers/getCourse';
+import { Course } from '../../types/course';
+import { SignUpCourse } from '../signUpCourse/SignUpCourse';
+import { Container, Divisor, FirstBody, SecondBody } from './styles';
 
 export const CourseDetail = () => {
+  const navigate = useNavigate();
+  const { name } = useParams();
+  const { user } = useUser();
+  const [course, setCourse] = useState({} as Course);
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    if (name) {
+      getCourse({ name }).then(data => setCourse(data as any));
+    }
+  }, [name]);
+
+  const handleDeleteCourse = async () => {
+    try {
+      await deleteCourse(parseInt(course.id, 10));
+      navigate('/meus-cursos');
+      alert('Curso deletado');
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
+
+  const isOwnerTeacher = user?.id === course.teacherId;
+
+  if (isOwnerTeacher && isEditing)
+    return <SignUpCourse editMode={true} courseToEdit={course} />;
+
   return (
     <Container>
       <HeaderUser />
-      <div className='detail-page-title'>
+      <div className="detail-page-title">
         <h1>Detalhes do Curso</h1>
       </div>
       <Divisor>
         <FirstBody>
-          <div className='course-name-container'>
-            <h1>Nome do Curso</h1>
+          <div className="course-name-container">
+            <h1>{course.name}</h1>
           </div>
-          <div className='teacher-name-container'>
-            <p>Nome do Professor</p>
+          <div className="teacher-name-container">
+            <p>{course.teacher?.name || ''}</p>
           </div>
-          <div className='info-container'>
-            <div className='category-container'>
-              <span>Categoria</span>
-              <p>Nome da Categoria</p>
+          <div className="info-container">
+            <div className="category-container">
+              <span>Categorias</span>
+              <p>{course.contents?.join(',')}</p>
             </div>
-            <div className='duration-container'>
+            <div className="duration-container">
               <span>Duração</span>
-              <p>10h</p>
+              <p>{course.totalHours}h</p>
             </div>
           </div>
-          <div className='description-container'>
+          <div className="description-container">
             <span>Descrição</span>
             <div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quaerat officiis incidunt magni voluptate praesentium error accusamus animi ad reprehenderit nihil deserunt perferendis, quidem, non tempora obcaecati dolor vel magnam.</p>
+              <p>{course.description}</p>
             </div>
           </div>
         </FirstBody>
         <SecondBody>
-          <div className='image-container'></div>
-          <div className='price-container'><p>R$90,00</p></div>
-          <div className='button-container'>
-            <button>Matricular-se Agora!</button>
+          <div className="image-container"></div>
+          <div className="price-container">
+            <p>
+              {course.price?.toLocaleString('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </p>
+          </div>
+          <div className="button-container">
+            {isOwnerTeacher ? (
+              <button onClick={() => setIsEditing(true)}>Editar</button>
+            ) : (
+              <button>Matricular-se Agora!</button>
+            )}
+            {isOwnerTeacher && (
+              <Button onClick={handleDeleteCourse}>Excluir</Button>
+            )}
           </div>
         </SecondBody>
       </Divisor>
-      <div className='other-courses-title'>
-        <h1>Cursos Relacionados</h1>
-      </div>
-      <OtherCourses>
-        <Carousel>
-            <Carousel.Item>
-              <ul>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.6}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.6}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.9}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.4}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.0}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-              </ul>
-            </Carousel.Item>
-            <Carousel.Item>
-              <ul>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.6}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.6}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.9}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.4}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-                <li>
-                  <CoursesCard
-                    name="Calculo 1"
-                    teacher="Lucas Caldas"
-                    raiting={4.0}
-                    currentPrice={29.9}
-                    originalPrice={289.0}
-                  />
-                </li>
-              </ul>
-            </Carousel.Item>
-          </Carousel>
-      </OtherCourses>
     </Container>
-  )
-}
+  );
+};
