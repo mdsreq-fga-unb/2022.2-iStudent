33517d72b58c45a6c665320cab9ea337118fa015
bruno henrique <bh.bsb2011@hotmail.com>
2023-02-05 17:21:24 -0300

    feat: l;inting

--- backend/src/main/adapters/express/express-route.adapter.ts
+++ backend/src/main/adapters/express/express-route.adapter.ts
@@ -1,25 +1,26 @@
-import { Request, Response } from "express";
-import { Controller, HttpRequest } from "../../../controllers/protocols";
+import { Request, Response } from 'express';
+import { Controller, HttpRequest } from '../../../controllers/protocols';
 
 export const adaptRoute = (controller: Controller) => {
-    return async (req: Request, res: Response) => {
-        const httpRequest: HttpRequest = {
-            body: req.body,
-        }
-        if(req.user){
-            httpRequest.user = {
-                email: req.user.email || null,
-                role: req.user.role || null,
-                id: req.user.id || null
-            }
-        }
-        const httpResponse = await controller.handle(httpRequest)
-        if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
-            res.status(httpResponse.statusCode).json(httpResponse.body)
-        } else {
-            res.status(httpResponse.statusCode).json({
-                error: httpResponse.body.message
-            })
-        }
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body,
+      query: req.query,
+    };
+    if (req.user) {
+      httpRequest.user = {
+        email: req.user.email || null,
+        role: req.user.role || null,
+        id: req.user.id || null,
+      };
     }
-}
+    const httpResponse = await controller.handle(httpRequest);
+    if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
+      res.status(httpResponse.statusCode).json(httpResponse.body);
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message,
+      });
+    }
+  };
+};
